329cf53d75d259798399fba03f7a6b8ff380c848
donellgill <donell.gill@outlook.com>
2021-04-10 22:34:56 -0400

    built fetchDogs()

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: firebrick');
+
+function fetchDogs(){
+ return fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then(resp => resp.json())
+  .then(json => dogArray(json));
+}
+  
+function dogArray(dogs){
+    const imgUrl = document.getElementById('dog-image-container');
+    dogs.forEach(dog) => {
+        const img = document.createElement('img');
+        img.innerHTML = breeds.img;
+        div.appendChild('img');
+      
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetchDogs();
+  });
+  
